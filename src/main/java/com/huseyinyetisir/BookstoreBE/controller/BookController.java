b4bb78316e7b8d3c5b73420b47e2dfff1b85c02a
Hüseyin Yetişir <huseynyetisir@gmail.com>
2023-11-19 23:08:45 +0300

    Edited for okta configuration bugs

--- src/main/java/com/huseyinyetisir/BookstoreBE/controller/BookController.java
+++ src/main/java/com/huseyinyetisir/BookstoreBE/controller/BookController.java
@@ -27,21 +27,21 @@ public class BookController {
     @GetMapping("/secure/ischeckedout/byuser")
     public Boolean checkoutBookByUser(@RequestHeader(value = "Authorization") String token,
             @RequestParam Long bookId) {
-        String userEmail = ExtractJWT.payloadJWTExtraction(token);
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkoutBookByUser(userEmail, bookId);
     }
 
     @GetMapping("/secure/checkout")
     public Book checkoutBook(@RequestHeader(value = "Authorization") String token,
             @RequestParam Long bookId) throws Exception {
-        String userEmail = ExtractJWT.payloadJWTExtraction(token);
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.checkoutBook(userEmail, bookId);
 
     }
 
     @GetMapping("/secure/currentloans/count")
     public int currentloansCount(@RequestHeader(value = "Authorization") String token) {
-        String userEmail = ExtractJWT.payloadJWTExtraction(token);
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
         return bookService.currentLoansCount(userEmail);
     }
 
