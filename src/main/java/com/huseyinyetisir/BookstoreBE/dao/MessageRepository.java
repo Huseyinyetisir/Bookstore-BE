5d9c64e727695818ba18bba5229d64775df4fd96
Hüseyin Yetişir <huseynyetisir@gmail.com>
2024-01-05 20:38:16 +0300

    Added get messages by user email func

--- src/main/java/com/huseyinyetisir/BookstoreBE/dao/MessageRepository.java
+++ src/main/java/com/huseyinyetisir/BookstoreBE/dao/MessageRepository.java
@@ -1,9 +1,14 @@
 package com.huseyinyetisir.BookstoreBE.dao;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.huseyinyetisir.BookstoreBE.entity.Message;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
+
+    Page<Message> findByUserEmail(@RequestParam("user_email") String userEmail, Pageable pageable);
 
 }
